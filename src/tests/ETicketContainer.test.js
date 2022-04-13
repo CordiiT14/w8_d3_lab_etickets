cf7be263907eb8f8ec5a39c8d6869725c1e5419c
CordiiT14 <cordelia.toennies@live.com>
2022-04-13 18:37:18 +0100

    three unit tests on modal complete

--- src/tests/ETicketContainer.test.js
+++ src/tests/ETicketContainer.test.js
@@ -2,6 +2,7 @@ import ETicketContainer from "../containers/eTicketContainer";
 import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
 import {shallow, configure } from 'enzyme';
+import ReactModal from 'react-modal';
 
 describe("ETicketContainer", () => {
 
@@ -12,8 +13,18 @@ describe("ETicketContainer", () => {
         container = shallow(<ETicketContainer/>)
     });
 
-    it('should render <ReactModal/>' , () => {
-        
+    it('Modal should render' , () => {
+        expect(container.find(ReactModal)).toHaveLength(1);
+    });
+
+    it('Modal prop isOpen is true', () => {
+        expect(container.find(ReactModal).prop('isOpen')).toBe(true);
+    });
+
+    it('Modal prop should change on button click', () => {
+        expect(container.find(ReactModal).prop('isOpen')).toBe(true);
+        container.find('button').simulate('click');
+        expect(container.find(ReactModal).prop('isOpen')).toBe(false);
     });
 
 })
